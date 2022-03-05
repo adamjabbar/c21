b844991724f85ec0bfec7d076b6aca2620b5513d
adamjabbar <94490498+adamjabbar@users.noreply.github.com>
2022-03-05 13:30:33 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var ball;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1250, 950);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+var ball_options={
+	isStatic:false,
+	restitution:0.3,
+	friction:0,
+	density:1.2
+}
+ball=Bodies.circle(260,100,20,ball_options)
+World.add(world,ball)
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  ellipse(ball.position.x,ball.position.y,20)
+  drawSprites();
+ 
+}
+
+
+
